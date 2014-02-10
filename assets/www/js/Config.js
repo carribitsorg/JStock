d9537ca33e5a3a825da25843e1274f250770c79d
carribits <carribits@gmail.com>
2014-02-09 23:09:05 -0500

    re layout application for small screen size

--- assets/www/js/Config.js
+++ assets/www/js/Config.js
@@ -1,6 +1,6 @@
 define([], function() {
     return {
-        baseurl: 'http://localhost/jstock',
+        baseurl: 'http://192.168.0.4/jstock',
         stockDate: '02/07/2014'
     };
 });
--- src/org/stock/MainActivity.java
+++ src/org/stock/MainActivity.java
@@ -2,6 +2,7 @@ package org.stock;
 import org.apache.cordova.DroidGap;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 
 public class MainActivity extends DroidGap
 {
@@ -10,5 +11,16 @@ public class MainActivity extends DroidGap
     {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+        
+        /* lock scaling in android 2.2 */
+        /* appView.getSettings().setSupportZoom(false);
+        appView.getSettings().setUseWideViewPort(false);
+        WebSettings ws = appView.getSettings();
+        ws.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
+        appView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
+        appView.setInitialScale(50);
+        ws.setSupportZoom(false);
+        ws.setBuiltInZoomControls(false);
+        ws.setUseWideViewPort(false); */
     }
 }

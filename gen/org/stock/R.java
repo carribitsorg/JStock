35f79d6ed73a0dab0d842216a93477439179fed0
carribits <carribits@gmail.com>
2014-03-06 01:57:07 -0500

    remove widget 2

--- gen/org/stock/R.java
+++ gen/org/stock/R.java
@@ -14,21 +14,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int appwidget_bg=0x7f020000;
-        public static final int appwidget_dark_bg=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int ic_sync_button=0x7f020003;
-        public static final int icon=0x7f020004;
-        public static final int main_header_selector=0x7f020005;
-        public static final int splash=0x7f020006;
-        public static final int widget=0x7f020007;
-    }
-    public static final class id {
-        public static final int buttonContainer=0x7f070000;
-        public static final int contentContainer=0x7f070002;
-        public static final int desc=0x7f070004;
-        public static final int sync_button=0x7f070001;
-        public static final int title=0x7f070003;
+        public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int main_header_selector=0x7f020002;
+        public static final int splash=0x7f020003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -39,6 +28,5 @@ public final class R {
     }
     public static final class xml {
         public static final int config=0x7f040000;
-        public static final int demo_widget_provider=0x7f040001;
     }
 }
